dabe750c5224d6d88d9eb7eb77f0860202f29426
Fernando Tsuda <ftsuda.senac@gmail.com>
2018-09-10 22:10:28 -0300

    Versão inicial Spring MVC

---
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/controller/ContatoController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplosspring.controller;
+
+import br.senac.tads.dsw.exemplosspring.model.Contato;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+@Controller
+@RequestMapping("/contato")
+public class ContatoController {
+
+    @GetMapping("/entrada")
+    public ModelAndView entrada() {
+        return new ModelAndView("contato/entrada")
+                .addObject("contato", new Contato());
+    }
+
+    @PostMapping("/salvar")
+    public ModelAndView salvar(
+            @ModelAttribute("contato") /*@Valid*/ Contato contato,
+            /* BindingResult bindingResult,*/
+            RedirectAttributes redirectAtrib) {
+
+//        if (bindingResult.hasErrors()) {
+//            return new ModelAndView("contato/entrada");
+//        }
+        return new ModelAndView("/contato/resultado");
+
+//        // USANDO POST-REDIRECT-GET
+//        redirectAtrib.addFlashAttribute("contato", contato);
+//        return new ModelAndView("redirect:/contato/resultado");
+    }
+
+    @GetMapping("/resultado")
+    public ModelAndView mostrarResultado() {
+        return new ModelAndView("/contato/resultado");
+    }
+}
---
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/model/Contato.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplosspring.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+public class Contato implements Serializable {
+
+    private long id;
+
+    private String nome;
+    
+    private String biografia;
+
+    private String email;
+
+    private int idade;
+
+    private int sexo; // 0-feminino, 1-masculino
+
+    public Contato() {
+
+    }
+
+    public Contato(long id, String nome, String email, int idade, int sexo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+}
